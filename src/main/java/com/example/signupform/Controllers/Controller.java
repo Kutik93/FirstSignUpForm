18fb1a3afdfca6fee8b7f3e67f5445da4a826c30
Uzytkownik <kutik93@gmail.com>
2022-08-01 12:57:18 +0200

    Day 2: Created class (controller) and added method.

---
+++ src/main/java/com/example/signupform/Controllers/Controller.java
@@ -0,0 +1,44 @@
+package com.example.signupform.Controllers;
+
+import com.example.signupform.Utils.DB;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button button_login;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private TextField tf_password;
+
+    @FXML
+    private Button button_sign_up;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DB.logInUser(event, tf_username.getText(), tf_password.getText());
+            }
+        });
+        button_sign_up.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DB.changeScene(event, "sign-up.fxml", "Sign Up!", null, null);
+            }
+        });
+    }
+}
